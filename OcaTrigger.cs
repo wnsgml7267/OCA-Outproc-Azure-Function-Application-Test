14f921714bde51b73379242ebc1a7dfef234d315
dlwns7267 <dlwns7267@naver.com>
2023-07-23 08:56:51 +0900

    add : OcaTriiger Endpoint

--- OcaTrigger.cs
+++ OcaTrigger.cs
@@ -1,7 +1,10 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace oca_outproc_azure_functions_application
 {
@@ -15,6 +18,9 @@ namespace oca_outproc_azure_functions_application
         }
 
         [Function("OcaTrigger")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" }, Summary = "The name of the person to use in the greeting.", Description = "This HTTP triggered function returns a person's name.", Visibility = OpenApiVisibilityType.Important)]
+        
+        
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
