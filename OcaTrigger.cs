3271d74f25afead510164d4636d7a605a73cbad6
dlwns7267 <dlwns7267@naver.com>
2023-07-23 09:00:16 +0900

    add : define response body

--- OcaTrigger.cs
+++ OcaTrigger.cs
@@ -20,6 +20,7 @@ namespace oca_outproc_azure_functions_application
         [Function("OcaTrigger")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" }, Summary = "The name of the person to use in the greeting.", Description = "This HTTP triggered function returns a person's name.", Visibility = OpenApiVisibilityType.Important)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "The name of the person to use in the greeting.", Description = "The name of the person to use in the greeting.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiResponseWithBody(statusCode: System.Net.HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Summary = "The response", Description = "This returns the response")]
         
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
